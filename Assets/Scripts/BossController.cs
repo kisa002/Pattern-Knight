39a35e84efb93ec470697428fc78d97c83221415
kisa002 <vnycall74@naver.com>
2018-12-20 18:41:03 +0900

    체력, 타이머 구현

---
+++ Assets/Scripts/BossController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController : MonoBehaviour
+{
+    public int currentHP, maxHP;
+
+    public int ad;
+
+    private void Start()
+    {
+        currentHP = maxHP;
+        UIManager.Instance.InitBoss(currentHP, maxHP);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            HitDmg(Random.Range(100, 500));
+    }
+
+    #region HP
+    public int GetHP()
+    {
+        return currentHP;
+    }
+
+    public void HitDmg(int dmg)
+    {
+        currentHP -= dmg;
+        UIManager.Instance.SetBossHP(currentHP, maxHP);
+
+        if (currentHP <= 0)
+        {
+            // Dead
+        }
+    }
+    #endregion
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance;
+
+    public Text textBossHP, textBossTime;
+    public Text textPlayerTime;
+
+    public Slider sliderBossHP;
+
+    private void Awake()
+    {
+        if (UIManager.Instance == null)
+            UIManager.Instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        StartPlayerTimer(15);
+        StartBossTimer(10);
+    }
+
+    #region Boss
+    public void InitBoss(int currentHP, int maxHP)
+    {
+        SetBossHP(currentHP, maxHP);
+
+        sliderBossHP.value = currentHP;
+        sliderBossHP.maxValue = maxHP;
+    }
+
+    public void SetBossHP(int currentHP, int maxHP)
+    {
+        textBossHP.text = currentHP + "/" + maxHP;
+
+        sliderBossHP.value = currentHP;
+    }
+    #endregion
+
+    #region Timer
+    public void StartPlayerTimer(int time)
+    {
+        StartCoroutine(CorPlayerTimer(time));
+    }
+
+    IEnumerator CorPlayerTimer(float time)
+    {
+        while(time >= 0)
+        {
+            for (int i = 10; i >= 0; i--)
+            {
+                time -= 0.1f;
+                textPlayerTime.text = string.Format("{0:00.0}", time);
+                textPlayerTime.transform.parent.GetComponent<Text>().text = textPlayerTime.text;
+
+                //Debug.Log(time.ToString());
+
+                if (time <= 0)
+                    break;
+                
+                yield return new WaitForSeconds(.1f);
+            }
+        }
+    }
+
+    public void StartBossTimer(float time)
+    {
+        StartCoroutine(CorBossTimer(time));
+    }
+
+    IEnumerator CorBossTimer(float time)
+    {
+        while (time >= 0)
+        {
+            for (int i = 10; i >= 0; i--)
+            {
+                time -= 0.1f;
+                textBossTime.text = string.Format("{0:00.0}", time);
+
+                if (time <= 0)
+                    break;
+
+                yield return new WaitForSeconds(.1f);
+            }
+        }
+    }
+    #endregion
+}
