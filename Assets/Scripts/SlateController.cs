1cc031a776d8e92106a8fe783f7323cc72c3d702
kisa002 <vnycall74@naver.com>
2018-12-21 02:46:20 +0900

    레이어 단계 조절 및 보스 게이지 추가

--- Assets/Scripts/SlateController.cs
+++ Assets/Scripts/SlateController.cs
@@ -36,7 +36,31 @@ public class SlateController : MonoBehaviour
 
     public void IncreaseGauge()
     {
-        imgSlatGuage.fillAmount += 0.105f;
+        //imgSlatGuage.fillAmount += 0.105f;
+
+        if (imgSlatGuage.fillAmount == 0)
+            imgSlatGuage.fillAmount = 0.097f;
+        else if (imgSlatGuage.fillAmount == 0.097f)
+            imgSlatGuage.fillAmount = 0.215f;
+        else if (imgSlatGuage.fillAmount == 0.215f)
+            imgSlatGuage.fillAmount = 0.336f;
+        else if (imgSlatGuage.fillAmount == 0.336f)
+            imgSlatGuage.fillAmount = 0.454f;
+        else if (imgSlatGuage.fillAmount == 0.454f)
+            imgSlatGuage.fillAmount = 0.57f;
+        else if (imgSlatGuage.fillAmount == 0.57f)
+            imgSlatGuage.fillAmount = 0.69f;
+        else if (imgSlatGuage.fillAmount == 0.69f)
+            imgSlatGuage.fillAmount = 0.806f;
+        else if (imgSlatGuage.fillAmount == 0.806f)
+            imgSlatGuage.fillAmount = 0.927f;
+        else if (imgSlatGuage.fillAmount == 0.927f)
+            imgSlatGuage.fillAmount = 1;
+        else
+        {
+            //꽉찬거임
+        }
+       
     }
 
     public void ChangeSlate()
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -19,6 +19,12 @@ public class UIManager : MonoBehaviour
 
     public Sprite sprStatOn, sprStatOff;
 
+    public GameObject slatePlayer, slateBoss;
+
+    public Image imgSlateBossGuage;
+    public Image[] imgBossSlates = new Image[9];
+    public Sprite[] sprSlates = new Sprite[9];
+
     string talkTitle = "대마왕";
     string[] talkContext = { "음냐음냐음...", "가소로운 것", "숨을 수 없는 공포를 맞이하라", "겨우 이정도로 나를 물리치려 하다니", "더... 더... 더 강하게 공격해보거라!", "지옥이 그대를 기다린다", "공허 그 너머로 너를 데려가주마", "솔로인가? 그렇다면 목숨만은 살려주지", "커플이라? 우주 최강의 고통을 안겨주마", "나는 아직 배가 고프다", "어떠한 공포를 선사해줄까", "아잉 때리지마여" };
 
@@ -112,13 +118,64 @@ public class UIManager : MonoBehaviour
     {
         float time = .5f;
 
-        for (int i = 0; i < 20; i++)
+        for (int i = 0; i < 22; i++)
         {
             imgBossGuage.fillAmount += 0.0415f;
             yield return new WaitForSeconds(time);
         }
+
+        BossAttack();
+    }
+
+    public void BossAttack()
+    {
+        // 보스가 플레이어에게 공격하는 함수
+        // 여기다가 보스 족보 보여주면 될듯
+
+        ShowSlateBoss();
     }
     #endregion
+
+    public void ShowSlatePlayer()
+    {
+        slatePlayer.SetActive(true);
+        slateBoss.SetActive(false);
+    }
+
+    public void ShowSlateBoss()
+    {
+        slatePlayer.SetActive(false);
+        slateBoss.SetActive(true);
+    }
+
+    public void IncreaseGauge()
+    {
+        //imgSlatGuage.fillAmount += 0.105f;
+
+        if (imgSlateBossGuage.fillAmount == 0)
+            imgSlateBossGuage.fillAmount = 0.097f;
+        else if (imgSlateBossGuage.fillAmount == 0.097f)
+            imgSlateBossGuage.fillAmount = 0.215f;
+        else if (imgSlateBossGuage.fillAmount == 0.215f)
+            imgSlateBossGuage.fillAmount = 0.336f;
+        else if (imgSlateBossGuage.fillAmount == 0.336f)
+            imgSlateBossGuage.fillAmount = 0.454f;
+        else if (imgSlateBossGuage.fillAmount == 0.454f)
+            imgSlateBossGuage.fillAmount = 0.57f;
+        else if (imgSlateBossGuage.fillAmount == 0.57f)
+            imgSlateBossGuage.fillAmount = 0.69f;
+        else if (imgSlateBossGuage.fillAmount == 0.69f)
+            imgSlateBossGuage.fillAmount = 0.806f;
+        else if (imgSlateBossGuage.fillAmount == 0.806f)
+            imgSlateBossGuage.fillAmount = 0.927f;
+        else if (imgSlateBossGuage.fillAmount == 0.927f)
+            imgSlateBossGuage.fillAmount = 1;
+        else
+        {
+            //꽉찬거임
+        }
+
+    }
 
     public void StatOn()
     {
