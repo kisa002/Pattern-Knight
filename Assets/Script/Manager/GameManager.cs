0b3b853f00be13ee125294d310526ca00aa83ee4
Myorang - LAB <eoeh92@naver.com>
2018-12-20 21:00:58 +0900

    유니티 에디터에서 작동하는 조작계 추가, 노트 체인 기능 추가, 족보 제너레이터 및 로더 구현, 족보와 노트 비교 구현

---
+++ Assets/Script/Manager/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public enum GameState
+{
+    Undefine,
+    Idle,
+    CanTouch,
+    None,
+}
+
+public class GameManager : Singleton<GameManager>
+{
+    public GameState m_GamteState = GameState.Idle;
+
+    public void Start()
+    {
+        SlateInfoLoader.Instance.SaveOrLoad();
+
+        NoteManager.Instance.ChangeMonsterNotes();
+
+        m_GamteState = GameState.CanTouch;
+    }
+}
---
+++ Assets/Script/Object/NoteCtrl.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoteCtrl : MonoBehaviour
+{
+    public SpriteRenderer m_Renderer;
+
+    public NoteType m_Type = NoteType.Undefine;
+    public GameObject touchBorder = null;
+    public Sprite[] noteSprites = new Sprite[9];
+
+    public void Active(NoteType type)
+    {
+        m_Type = (NoteType)Random.Range(0, 9);
+
+        gameObject.SetActive(true);
+
+        m_Renderer.sprite = noteSprites[(int)m_Type];
+    }
+
+    public void Touched()
+    {
+        touchBorder.SetActive(true);
+    }
+
+    public void UnTouched()
+    {
+        touchBorder.SetActive(false);
+    }
+
+    public void Destroy()
+    {
+        gameObject.SetActive(false);
+        touchBorder.SetActive(false);
+    }
+}
